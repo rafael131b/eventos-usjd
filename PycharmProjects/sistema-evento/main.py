2e0c3f0c1f7e55957e567c13e8565d48d6ee7898
rafael <rafa.timoteo.c@gmail.com>
2023-12-02 10:36:55 -0300

    adicionando novas funcoes

--- PycharmProjects/sistema-evento/main.py
+++ PycharmProjects/sistema-evento/main.py
@@ -1,28 +1,59 @@
 from Models.EventoModel import EventoModel
 from Models.UsuarioModel import UsuarioModel
-
 from View.EventoView import EventoView
 from Controller.EventoController import EventoController
+from Controller.UsuarioController import UsuarioController
+from Services.server import SimpleServer
 
-
-# Criar instâncias
 modelo = EventoModel()
 visualizacao = EventoView()
 controle = EventoController(modelo, visualizacao)
 
-# Adicionar evento usando o controlador
-controle.adicionar_evento(1, "Evento  Homenagem ao Pelé", "rua Vila Belmiro", "esporte", None, "Homenagem o Rei Pelé na VIla Belimiro")
-#controle.adicionar_evento(2, "andre", "rua jose santos", "esporte", None, "sem detalhes")
-# Exemplo de uso
-controle.mostrar_detalhes_evento(2)
+controle.adicionar_evento(1, "Evento Homenagem ao Pelé", "rua Vila Belmiro", "esporte", None, "Homenagem o Rei Pelé na Vila Belmiro")
+
+controle.mostrar_detalhes_evento(1)
+
+def criar_usuario(nome, idade, email, cpf):
+    print("Você deseja criar um Usuario")
+    print(f"Nome: {nome}, Idade: {idade}, Email: {email}, CPF: {cpf}")
+    usuario = UsuarioModel(nome=nome, idade=idade, email=email, id=cpf)
+    print(f"NOVO USUARIO CRIADO COM SUCESSO! {usuario.nome} está em nossa base de dados")
+
+def criar_evento():
+    print("criar evento")
+    idEvento=int(input("digite o ID"))
+    nomeEvento = input("digite o Nome do Evento")
+    endereco=input("digite o endereco")
+    categoria=input("digite a categoria")
+    horario=input("digite o horario")
+    descricao = input("digite a descricao")
+    controle.adicionar_evento(idEvento, nomeEvento, endereco, categoria, horario,descricao)
+    print(f"Evento: {nomeEvento} FOI CRIADO COM SUCESSO")
 
 
-usuario = UsuarioModel(nome="Davi", idade=32, email="daviSantos@gmail.com", id=1)
 
-controle.adicionar_usuario_ao_evento(1,usuario.nome)
-print("todos eventos",controle.obter_todos_eventos())
+def ver_eventos():
+    pass
 
+def ver_usuarios():
+    # Implemente a lógica para ver todos os usuários aqui
+    pass
 
+print("Selecione o que você pretende fazer")
+print("Digite 1 para criar um Usuario, digite 2 para criar um Evento, digite 3 para ver todos os eventos e digite 4 para ver todos usuarios")
+valor_input = int(input())
 
-
-
+if valor_input == 1:
+    nome = input("Digite o Nome do Usuário que deseja criar: ")
+    idade = int(input("Digite a Idade: "))
+    email = input("Digite o Email: ")
+    cpf = input("Digite o CPF: ")
+    criar_usuario(nome, idade, email, cpf)
+elif valor_input == 2:
+    criar_evento()
+elif valor_input == 3:
+    ver_eventos()
+elif valor_input == 4:
+    ver_usuarios()
+else:
+    print("Opção inválida")
